367d12da03e90018b678e50b214cc03d4bc2b16e
retrovanya <ivanvelikodny2001@gmail.com>
2023-07-13 23:54:29 +0300

    add model with thymeleaf

--- src/main/java/springmvc/controllers/FirstController.java
+++ src/main/java/springmvc/controllers/FirstController.java
@@ -1,11 +1,11 @@
 package springmvc.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @RequestMapping("/first")
@@ -13,8 +13,10 @@ public class FirstController {
 
     @GetMapping("/hello")
     public String helloPage(@RequestParam(value = "name", required = false) String name,
-                            @RequestParam(value = "surname", required = false) String surname){
-        System.out.println("Hello, " + name + " " + surname);
+                            @RequestParam(value = "surname", required = false) String surname,
+                            Model model){
+        //System.out.println("Hello, " + name + " " + surname);
+        model.addAttribute("message", "Hello, " + name + " " + surname );
         return "first/hello";
     }
 
