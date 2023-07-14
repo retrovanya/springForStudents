23745237622700e1559082871b527ee34f79ea3b
retrovanya <ivanvelikodny2001@gmail.com>
2023-07-14 16:22:44 +0300

    add index method

--- src/main/java/springmvc/controllers/PeopleController.java
+++ src/main/java/springmvc/controllers/PeopleController.java
@@ -1,19 +1,28 @@
 package springmvc.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import springmvc.dao.PersonDAO;
 
 @Controller
 @RequestMapping("/people")
 public class PeopleController {
 
+    private final PersonDAO personDAO;
+
+    @Autowired
+    public PeopleController(PersonDAO personDAO) {
+        this.personDAO = personDAO;
+    }
+
     @GetMapping
     public String index(Model model) {
-        //получим людей из DAO и передадим на отображение в представлении
-        return null;
+        model.addAttribute("people", personDAO.index());
+        return "/people/index";
     }
 
     @GetMapping("/{id}")
--- src/main/java/springmvc/dao/PersonDAO.java
+++ src/main/java/springmvc/dao/PersonDAO.java
@@ -1,10 +1,12 @@
 package springmvc.dao;
 
+import org.springframework.stereotype.Component;
 import springmvc.models.Person;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Component
 public class PersonDAO {
     private static int PEOPLE_COUNT; //id
     private List<Person> people;
@@ -22,7 +24,7 @@ public class PersonDAO {
         return people;
     }
 
-    public Person show(int id){
-        return people.stream().filter(person -> person.getId()==id).findAny().orElse(null); //ищем по id
+    public Person show(int id) {
+        return people.stream().filter(person -> person.getId() == id).findAny().orElse(null); //ищем по id
     }
 }
