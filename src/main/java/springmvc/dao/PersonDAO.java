84c861ed6f155199e9175c6a9ca91be2dfbb9daa
retrovanya <ivanvelikodny2001@gmail.com>
2023-07-14 16:16:26 +0300

    add methods to DAO

--- src/main/java/springmvc/dao/PersonDAO.java
+++ src/main/java/springmvc/dao/PersonDAO.java
@@ -1,4 +1,28 @@
 package springmvc.dao;
 
+import springmvc.models.Person;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class PersonDAO {
+    private static int PEOPLE_COUNT; //id
+    private List<Person> people;
+
+    {
+        people = new ArrayList<>();
+
+        people.add(new Person(++PEOPLE_COUNT, "Ivan"));
+        people.add(new Person(++PEOPLE_COUNT, "Egor"));
+        people.add(new Person(++PEOPLE_COUNT, "Maxim"));
+        people.add(new Person(++PEOPLE_COUNT, "Petr"));
+    }  //блок инициализации
+
+    public List<Person> index() {
+        return people;
+    }
+
+    public Person show(int id){
+        return people.stream().filter(person -> person.getId()==id).findAny().orElse(null); //ищем по id
+    }
 }
