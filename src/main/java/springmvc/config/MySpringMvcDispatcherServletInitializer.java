ebc3ebb1d48d2b71fddea3c47cdf8f57cb134c3a
retrovanya <ivanvelikodny2001@gmail.com>
2023-07-15 15:25:33 +0300

    add tools for hidden method

--- src/main/java/springmvc/config/MySpringMvcDispatcherServletInitializer.java
+++ src/main/java/springmvc/config/MySpringMvcDispatcherServletInitializer.java
@@ -1,6 +1,12 @@
 package springmvc.config;
 
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import javax.servlet.Filter;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
 
 public class MySpringMvcDispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
     @Override
@@ -16,5 +22,11 @@ public class MySpringMvcDispatcherServletInitializer extends AbstractAnnotationC
     @Override
     protected String[] getServletMappings() {
         return new String[]{"/"};  //Все запросы от пользователя посылаются на DispatcherServlet
+    }
+
+    @Override
+    protected Filter[] getServletFilters() {
+        return new Filter[] {
+                new HiddenHttpMethodFilter(), new CharacterEncodingFilter() };
     }
 }
